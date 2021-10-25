5fb4f4c2f2f15f2a70e2f56756c7c04290aa6670
Jessica Blankemeier <jblankemeier@tandemdiabetes.com>
2021-10-24 20:35:45 -0700

    Wireframe in place for test form

--- src/Signature.js
+++ src/Signature.js
@@ -1,8 +1,6 @@
 import React, {useState} from 'react';
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import Container from 'react-bootstrap/Container';
-import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Row';
@@ -10,33 +8,21 @@ import Col from 'react-bootstrap/Row';
 const Signature = () => {
     const [startDate, setStartDate] = useState(new Date());
   return (
-      <Container>
-              <Row>
-              <FloatingLabel controlID="floatingSummary" label="Summary">
-              <Form.Select aria-label="Test Results">
-                  <option> Test Results</option>
-                  <option value="1">Pass</option>
-                  <option value="2">Fail</option>
-                  <option value="3">Protocol Fail</option>
-                  <option value="4">N/A</option>
-              </Form.Select>
-              </FloatingLabel>
-              </Row>
-              <Form>
+          <Form>
               <Row className="mb-3">
-                <Form.Group as={Col} controlID="formGridName" >
+                  <Col xs={5}>
                 <Form.Label>Test Performed By</Form.Label>
                 <Form.Control type="text" placeholder="Tester Name" />
-                </Form.Group>
-                <Form.Group as={Col} controlID="formGridSig" >
+                </Col>
+                <Col xs={5}>
                 <Form.Label>Signature</Form.Label>
                 <Form.Control type="text" />
-                </Form.Group>
-                
+               </Col>
+               <Col>
             <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
+            </Col>
             </Row>
           </Form>
-          </Container>
   )
 }
 export default Signature;
---
+++ src/Summary.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Row';
+
+const Summary = () => {
+  return (
+      <Container>
+          <Form>
+              <Row className="mb-3">
+                <Col xs={3}>
+              <FloatingLabel controlID="floatingSummary" label="Summary">
+              <Form.Select aria-label="Test Results">
+                  <option> Test Results</option>
+                  <option value="1">Pass</option>
+                  <option value="2">Fail</option>
+                  <option value="3">Protocol Fail</option>
+                  <option value="4">N/A</option>
+              </Form.Select>
+              </FloatingLabel>
+              </Col>
+              </Row>
+              </Form>
+          </Container>
+  )
+}
+export default Summary;
